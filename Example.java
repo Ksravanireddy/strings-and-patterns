89c5f0a029497e6be982e2f76e253ef3ba1d9e1f
ksravanireddy <sravanir087@gmail.com>
2021-12-27 19:13:59 +0530

    commit

---
+++ Example.java
@@ -0,0 +1,10 @@
+public class Example2{
+    public static void main(String[] args){
+        String Str = "sravani";
+        char ch = str.charAT(4);
+        System.out.println(ch);
+        System.out.println("the length of the string is " +str.length());
+        
+
+    }
+}
