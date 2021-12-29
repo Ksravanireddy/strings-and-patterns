51505d016e99fe58ad1dcbc59b33e6586f29972a
ksravanireddy <sravanir087@gmail.com>
2021-12-29 18:40:14 +0530

    committed

---
+++ Formate.java
@@ -0,0 +1,9 @@
+public class Formate{
+    public static void main(String[] args){
+    String name = "sravani";
+    String sf1 = String.format("name is %s",name);
+    System.out.println(sf1);
+    
+
+    }
+}
