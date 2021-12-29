5c509ce4d3170183d4b58429cd595089c4bd654b
ksravanireddy <sravanir087@gmail.com>
2021-12-29 18:27:41 +0530

    commited

---
+++ Substring.java
@@ -0,0 +1,7 @@
+public class Substring{
+public static void main(String[] args){
+String str1 = "sravanireddy";
+System.out.println(str1.substring(0));
+System.out.println(str1.substring(1,7));
+}
+}
