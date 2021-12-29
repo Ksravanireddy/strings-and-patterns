3d2514cb5c28ebb9bc7f88bf9d971099c2a82f3a
ksravanireddy <sravanir087@gmail.com>
2021-12-29 18:44:19 +0530

    commited

---
+++ Boolean.java
@@ -0,0 +1,7 @@
+public class Boolean{
+    public static void main(String[] args){
+        String name = "i love to sing";
+        System.out.println(name.contains("sing love"));
+        System.out.println(name.contains("love to sing"));
+    }
+}
